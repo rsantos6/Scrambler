dab956da33975bf4e3f6102ee8689dbf0c1c2419
Russell Santos <russellandshira@msn.com>
2017-10-18 21:41:26 -0400

    not pretty code at all. not particulary happy with the flow or neatness of the code. but i got a quiz tomorrow for another class so should probably be working on that instead of this

--- scrambler.py
+++ scrambler.py
@@ -3,23 +3,56 @@
 
 
 def main():
+    
     make_key_choice = input('Use existing key? (Y/N): ')
-
+    while make_key_choice not in ['Y', 'N']:
+        print(make_key_choice + ' is an invalid command')
+        make_key_choice = input('Use existing key? (Y/N): ')
+            
     if make_key_choice == 'N':
         scrambled = make_new_key()
     elif make_key_choice == 'Y':
         scrambled = get_old_key()
 
     write_message_bool = input('Write or read message? (write/read): ')
+    while write_message_bool not in ['write', 'read']:
+        print(write_message_bool + ' is an invalid command')
+        write_message_bool = input('Write or read message? (write/read): ')
 
     if write_message_bool == 'write':
         write_message(scrambled)
     elif write_message_bool == 'read':
         read_message(scrambled)
+
+def loop(scrambled):
+    go_again = input('Go again? (Y/N): ')
+    while go_again not in ['Y', 'N']:
+        print(go_again + ' is an invalid command')
+        go_again = input('Use existing key? (Y/N): ')
+    if go_again == 'Y':
+        use_same_key = input('Use same key? (Y/N): ')
+        while use_same_key not in ['Y', 'N']:
+            print(use_same_key + ' is an invalid command')
+            use_same_key = input('Use same key? (Y/N): ')
+        if use_same_key == 'Y':
+            read_write = input('Write or read message? (write/read): ')
+            while read_write not in ['write', 'read']:
+                print(read_write + ' is an invalid command')
+                read_write = input('Write or read message? (write/read): ')
+            if read_write == 'write':
+                write_message(scrambled)
+            elif read_write == 'read':
+                read_message(scrambled)
+        elif use_same_key == 'N':   
+            main()
     
 def get_old_key():
     key_choice = input('Enter key: ')
     result = firebase.get(key_choice, None)
+    while result is None:
+        print(key_choice + ' is not an existing key')
+        key_choice = input('Enter key: ')
+        result = firebase.get(key_choice, None)
     key_name_gen = (key for key in result)
     key_name = next(key_name_gen)
     return result.get(key_name)
@@ -49,6 +82,7 @@ def write_message(scrambled):
             corresponding_pair = next(corresponding_pair)
             new_message += corresponding_pair[1]
     print(new_message)
+    loop(scrambled)
 
 def read_message(scrambled):
     message = input('Enter the message you want to decode: ')
@@ -62,6 +96,7 @@ def read_message(scrambled):
             corresponding_pair = next(corresponding_pair)
             new_message += corresponding_pair[0]
     print(new_message)
+    loop(scrambled)
 
 if __name__ == '__main__':
     import random
